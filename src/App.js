be656be9f482061a3deaa9767ac3000dd21f9995
Robert Síbek <robert.sibek@gmail.com>
2023-12-08 23:28:09 +0100

    added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import BookList from "./components/BookList";
 import AddBook from "./components/AddBook";
 
-// apollo client setup
+// apollo client setup new
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
